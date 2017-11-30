70995d650f4e257fec96b4d2d273af2f697a336d
Steve Ivy <steveivy@gmail.com>
2017-11-30 08:58:04 -0700

    Add basic setup.py
    Doing this on the fly on github and for py3 (which I don't have a lot of experience with) so I have no idea if I'm doing it right

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='mastodon_backup',
+    version='0.0.1',
+    description="Utility for backing up your Mastodon content.",
+    author="Alex Schroeder",
+    packages=["mastodon_backup"],
+    entry_points={
+        "console_scripts": ["mastodon_backup:mastodon_backup:main"]
+    },
+    install_requires=[
+        "mastodon.py",
+        "pysmartdl",
+        "progress",
+        "html2text",
+        
+    ],
+)
