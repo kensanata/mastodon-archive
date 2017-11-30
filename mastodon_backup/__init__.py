5eb00d9091591dd2675dde9bf12e13e8a59d4c76
Steve Ivy <steve.ivy@wellsfargo.com>
2017-11-30 11:08:28 -0700

    change the 'content' subcommand to 'archive'

--- mastodon_backup/__init__.py
+++ mastodon_backup/__init__.py
@@ -1,19 +1,20 @@
 import argparse
-import .backup
+from . import backup
+
 
 def main():
     parser = argparse.ArgumentParser("Mastodon Backup")
 
     subparsers = parser.add_subparsers()
 
-    parser_content = subparsers.add_parser('content')
+    parser_content = subparsers.add_parser('archive')
     parser_content.add_argument("user")
     parser_content.set_defaults(command=backup.backup)
 
     args = parser.parse_args()
 
-    args.command()
+    args.command(args)
+
 
 if __name__ == "__main__":
     main()
-    
