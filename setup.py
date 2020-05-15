b3188b6cbf4292247ad34c53d9cf42359796984a
Alex Schroeder <alex@gnu.org>
2020-05-15 09:25:10 +0200

    Prepare for release 1.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='mastodon_archive',
-    version='1.3.0',
+    version='1.3.1',
     description="Utility for backing up your Mastodon content",
     long_description=long_description,
     long_description_content_type="text/markdown",
