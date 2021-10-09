af6ba0cc2b76c67536f5cda70dea672bb5c00200
Spring Raindrop <SpringRaindrop@vivaldi.net>
2021-10-09 19:35:25 +0000

    Decouple server logic from data processing and presentation

--- mastodon_archive/meow.py
+++ mastodon_archive/meow.py
@@ -59,6 +59,28 @@ def meow(args):
         else:
             return url
 
+    transform_media_urls(data, use_local_file_if_exists)
+    data["files"] = media_files
+
+    print("Please, open Meow at", meow_open_path, "to continue!")
+
+    file_cb = lambda *args: None
+
+    bar = None
+    if len(media_files) > 0:
+        bar = Bar("Exporting files", max = len(media_files) + 1)
+        file_cb = lambda *args: bar.next()
+
+    serve(server_port, meow_origin, data, media_dir, media_files, file_cb)
+
+    if bar:
+        bar.finish()
+    print("Export finished!")
+
+def transform_media_urls(data, func):
+    """
+    Calls func on each media file URL and sets the latter to the result.
+    """
     for collection in ["statuses", "favourites"]:
         for status in data[collection]:
             attachments = status["media_attachments"]
@@ -66,58 +88,46 @@ def meow(args):
                 attachments = status["reblog"]["media_attachments"]
             for attachment in attachments:
                 if attachment["url"]:
-                    attachment["url"] = use_local_file_if_exists(attachment["url"])
+                    attachment["url"] = func(attachment["url"])
 
     for picture in ["avatar", "header"]:
-        data["account"][picture] = use_local_file_if_exists(data["account"][picture])
+        data["account"][picture] = func(data["account"][picture])
 
-    data["files"] = media_files
-
-    import_complete = False
+def serve(port, origin, data, media_dir, media_files, file_cb):
+    complete = False
     def not_completed():
-        nonlocal import_complete
-        return not import_complete
-
-    bar = False
-    if len(media_files) > 0:
-        bar = Bar("Exporting files", max = len(media_files) + 1)
+        nonlocal complete
+        return not complete
 
     class Handler(http.server.BaseHTTPRequestHandler):
         def do_GET(self):
-            nonlocal import_complete
-            nonlocal bar
-
+            nonlocal complete
             query = parse_qs(urlparse(self.path).query)
 
             if self.path == "/":
                 self.send_response(200)
-                self.send_header("Access-Control-Allow-Origin", meow_origin)
+                self.send_header("Access-Control-Allow-Origin", origin)
                 self.send_header("Content-type", "application/json")
                 self.end_headers()
 
                 self.wfile.write(bytes(json.dumps(data), "utf-8"))
 
-                if bar:
-                    bar.next()
+                file_cb()
             elif "file" in query and query["file"][0] in media_files:
                 self.send_response(200)
-                self.send_header("Access-Control-Allow-Origin", meow_origin)
+                self.send_header("Access-Control-Allow-Origin", origin)
                 self.end_headers()
 
                 file_name = media_dir + query["file"][0]
                 with open(file_name, "rb") as file:
                     self.wfile.write(file.read())
 
-                if bar:
-                    bar.next()
+                file_cb()
             elif "complete" in query:
                 self.send_response(200)
                 self.end_headers()
 
-                import_complete = True
-
-                if bar:
-                    bar.finish()
+                complete = True
             else:
                 self.send_error(404)
 
@@ -125,9 +135,6 @@ def meow(args):
             return
 
     socketserver.TCPServer.allow_reuse_address = True
-    with socketserver.TCPServer(("127.0.0.1", server_port), Handler) as httpd:
-        print("Please, open Meow at", meow_open_path, "to continue!")
+    with socketserver.TCPServer(("127.0.0.1", port), Handler) as httpd:
         while not_completed():
             httpd.handle_request()
-
-    print("Export finished!")
