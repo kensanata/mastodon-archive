1c8de816c97bf03476a31ab354502f8ae968cbef
Alex Schroeder <alex@gnu.org>
2022-09-20 18:16:08 +0200

    Release 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='mastodon_archive',
-    version='1.4.1',
+    version='1.4.2',
     description="Utility for backing up your Mastodon content",
     long_description=long_description,
     long_description_content_type="text/markdown",
