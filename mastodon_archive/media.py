28fcbfc10bd4b47d29f098ce44c1af3fe1103df4
legogo29 <legogo29@users.noreply.github.com>
2025-01-27 18:22:50 +0000

    fix double counting of media files

--- mastodon_archive/media.py
+++ mastodon_archive/media.py
@@ -89,7 +89,6 @@ def media(args):
 
     # start downloading the missing files from the back
     for url in reversed(urls):
-        bar.next()
         remoteurl = url[1]
         url = url[0]
         if not args.quiet:
